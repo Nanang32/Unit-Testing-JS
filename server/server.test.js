5e555bd1c0ab2e59fd45c711860f555c2c3c47cb
luqman <luqman.fanani@gmail.com>
2019-01-13 10:33:37 +0800

    mengelompokkan test dengan describe

--- server/server.test.js
+++ server/server.test.js
@@ -1,31 +1,33 @@
 const request = require('supertest')
 const expect = require('expect')
-
 var app = require('./server.js').app
-it('should return index response',(done)=>{
-	request(app)
-	.get('/')
-	.expect(404)
-	.expect((res) =>{
-		expect(res.body).toInclude({
-			error: 'halaman tidak ditemukan'
-		})
-	})
-	.end(done)
-})
-// buat testing baru
-// assert 200
-// assertnama kamu dalam array
-it('should return users response',(done)=>{
-	request(app)
-	.get('/users')
-	.expect(200)
-	.expect((res)=>{
-		expect(res.body).toInclude({
-			nama: 'ardi',
-			umur: 33
-		})
-
-	})
-	.end(done)
+// case
+describe('#SERVER', () => {
+    describe('GET /', () => {
+        it('should return index response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'halaman tidak ditemukan'
+                    })
+                })
+                .end(done)
+        })
+    })
+    describe('GET /Users', () => {
+        it('should return users response', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        nama: 'ardi',
+                        umur: 33
+                    })
+                })
+                .end(done)
+        })
+    })
 })
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,32 +1,37 @@
 // mjackson expect
 const expect = require('expect')
 const utils = require('./utils')
-it('eksekusi dua angka', () => {
-    var res = utils.add(33, 53)
-    expect(res).toBe(86).toBeA('number')
+
+describe('Utils',()=>{
+    describe('#penjumlahan',()=>{
+        it('eksekusi dua angka', () => {
+            var res = utils.add(33, 53)
+            expect(res).toBe(86).toBeA('number')
+        })
+    })    
+    it('should asycn add two number',(done)=>{
+      utils.addAsyncAdd(5,5,(sum)=>{
+        expect(sum).toBe(10).toBeA('number')
+        done()
+      })
+    })
+    it('eksekusi asycn dua angka di kalikan', (done) => {
+        utils.perkalian(3,11,(sum)=>{
+        expect(sum).toBe(33).toBeA('number')
+        done()        
+        })
+    })
+    // selesaikan verifikasi nama bdepan dan belakang
+    it('should ver nama depan dan belakang', () => {
+        var user = { alamat: 'makassar', umur: 22 }
+        var res = utils.setName(user, 'Luqman Fanani')
+        expect(res).toInclude({
+            Namadepan: 'Luqman',
+            Namabelakang: 'Fanani'
+        })
+    })
 })
 
-it('should asycn add two number',(done)=>{
-  utils.addAsyncAdd(5,5,(sum)=>{
-    expect(sum).toBe(10).toBeA('number')
-    done()
-  })
-})
-it('eksekusi asycn dua angka di kalikan', (done) => {
-    utils.perkalian(3,11,(sum)=>{
-    expect(sum).toBe(33).toBeA('number')
-    done()        
-    })
-})
-// selesaikan verifikasi nama bdepan dan belakang
-it('should ver nama depan dan belakang', () => {
-    var user = { alamat: 'makassar', umur: 22 }
-    var res = utils.setName(user, 'Luqman Fanani')
-    expect(res).toInclude({
-        Namadepan: 'Luqman',
-        Namabelakang: 'Fanani'
-    })
-})
 // it('should expect some value ', () => {
 //   // expect(12).toNotBe(12)
 //   // expect({name:'NAnang'}).toNotEqual({name:'Nanang'})
